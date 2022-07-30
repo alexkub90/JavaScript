ca621488c64cda8dc548f75eac27bf8ac544c354
alexkub90 <alexkub90@gmail.com>
2022-07-30 22:29:38 +0500

    add text

--- example.js
+++ example.js
@@ -1,13 +1,41 @@
 //Методы объекта, "this"
+// const user = {
+//     name: 'Alex',
+//     age: 31,
+// };
+
+// function sayHi() {
+//     console.log('Hello')
+// }
+
+// user.sayHi = sayHi;
+
+// user.sayHi();
+
+//Сокращенная запись метода
+
+// user = {
+//     sayHi(){
+//         console.log('Hello, Alex');
+//     }
+// }
+
+// user.sayHi()
+
 const user = {
     name: 'Alex',
-    age: 31,
+};
+
+const admin = {
+    name: 'Boris',
 };
 
 function sayHi() {
-    console.log('Hello')
-}
+    console.log(this.name);
+};
 
-user.sayHi = sayHi;
+user.f = sayHi;
+admin.f = sayHi;
 
-user.sayHi();
+user.f();
+admin.f();
