23b07901cbe71c62c60f862ee5df65bc8da00fee
alexkub90 <alexkub90@gmail.com>
2022-07-31 22:21:33 +0500

    finish calculator.js

--- calculator.js
+++ calculator.js
@@ -5,35 +5,16 @@ let calculator = {
         this.a = +a;
         this.b = +b;
      },
+
     sum() {
         return this.a + this.b;
     },
+    
     mul() {
         return this.a * this.b;
     }
 };
 
-
-
- calculator.read();
+ calculator.read(); 
  alert(calculator.sum());
-//  alert(calculator.mul);
-
-// function sum() {
-//     return this.a + this.b;
-// };
-
-// function mul() {
-//     return this.b * this.b;
-// };
-
-// calculator.read();
-// alert(calculator.sum());
-// alert(calculator.mul());
-
-// function read(a, b) {
-//    a = prompt('a?');
-//    b = prompt('b?');
-//    calculator.a = a;
-//    calculator.b = b;
-// }
+ alert(calculator.mul());
