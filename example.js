fe6c33f90e4fe481aa0ddf241b5c03f545b9d1aa
alexkub90 <alexkub90@gmail.com>
2022-07-31 22:57:05 +0500

    edit example.js

--- example.js
+++ example.js
@@ -22,20 +22,42 @@
 
 // user.sayHi()
 
-const user = {
-    name: 'Alex',
-};
+// const user = {
+//     name: 'Alex',
+// };
 
-const admin = {
-    name: 'Boris',
-};
+// const admin = {
+//     name: 'Boris',
+// };
 
-function sayHi() {
-    console.log(this.name);
-};
+// function sayHi() {
+//     console.log(this.name);
+// };
 
-user.f = sayHi;
-admin.f = sayHi;
+// user.f = sayHi;
+// admin.f = sayHi;
 
-user.f();
-admin.f();
+// user.f();
+// admin.f();
+
+let ladder = {
+    step: 0
+  };
+
+  function up() {
+    this.step++;
+  };
+  
+  function down() {
+    this.step--;
+  };
+  
+  function showStep() { // показывает текущую ступеньку
+    alert( this.step );
+  }
+
+  ladder.up = up;
+  ladder.down = down;
+  ladder.showStep = showStep;
+  
+  ladder.up()
