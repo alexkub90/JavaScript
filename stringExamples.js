f2fb5685ad5c7a633917fdcb93eb3a1cc1eff2ab
alexkub90 <alexkub90@gmail.com>
2022-08-08 22:39:15 +0500

    edit stringExamples.js

--- stringExamples.js
+++ stringExamples.js
@@ -22,3 +22,16 @@ function checkSpam(str) {
 }
 
 checkSpam('VIaGra XxxxX , XX')
+
+//Функция усечения строки
+
+function truncate(str, maxlength) {
+    
+    if (str.length > maxlength) {
+        return str.slice(0, maxlength - 1) + '…';
+    }
+
+    return str;
+}
+
+truncate('123456789012345678901234567890', 20)
