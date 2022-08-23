25a91e8fdcc79ffa878428f23fb93f3057521430
alexkub90 <alexkub90@gmail.com>
2022-08-23 22:41:28 +0500

    format calculatorThree.js

--- calculatorThree.js
+++ calculatorThree.js
@@ -1,34 +1,33 @@
 function Calculator() {
-//  Добавил метод который определяет оператор и производит действие.
-  this.methods = { 
+  //  Добавил метод который определяет оператор и производит действие.
+  this.methods = {
     "+": (a, b) => a + b,
-    "-": (a, b) => a - b
+    "-": (a, b) => a - b,
   };
 
-// Метод расщепляет строку на a, op(operator), проверяет на NaN, если false, то производит действие this.methods.
+  // Метод расщепляет строку на a, op(operator), проверяет на NaN, если false, то производит действие this.methods.
   this.calculate = function (str) {
-
     let split = str.split(" "),
       a = +split[0],
       op = split[1],
       b = +split[2];
-// Проверка есть ли такой оператор и являются ли a, b числами.
+    // Проверка есть ли такой оператор и являются ли a, b числами.
     if (!this.methods[op] || isNaN(a) || isNaN(b)) {
       return NaN;
     }
-// Переход в метод в самом начале.
+    // Переход в метод в самом начале.
     return this.methods[op](a, b);
-  }
-// Добавил метод который добавляет новый оператор и действия с этим оператором.
+  };
+  // Добавил метод который добавляет новый оператор и действия с этим оператором.
   this.addMethod = function (name, func) {
     this.methods[name] = func;
   };
 }
 
-let powerCalc = new Calculator;
+let powerCalc = new Calculator();
 powerCalc.addMethod("*", (a, b) => a * b);
 powerCalc.addMethod("/", (a, b) => a / b);
 powerCalc.addMethod("**", (a, b) => a ** b);
 
 let result = powerCalc.calculate("2 ** 3");
-alert( result ); // 8
+alert(result); // 8
